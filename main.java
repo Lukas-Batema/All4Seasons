1972c180caf484400b0002028d28d6727eaa763d
Lukas Batema <lukasbatema@gmail.com>
2021-02-08 12:05:51 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,19 @@
-
+public class main {
+  public static void main(String[] args) {
+    static void summer() {
+      System.out.println("This season is not ready yet, please check back later!");
+    }
+    
+    static void spring() {
+      System.out.println("This season is not ready yet, please check back later!");
+    }
+    
+    static void autumn() {
+      System.out.println("This season is not ready yet, please check back later!");
+    }
+    
+    static void winter() {
+      System.out.println("This season is not ready yet, please check back later!");
+    }
+  }
+}
