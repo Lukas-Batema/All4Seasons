60f948bc0f256e69a400bbd86f8a2b96921f8c3f
Lukas Batema <lukasbatema@gmail.com>
2021-02-08 12:49:53 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
 public class main {
   public static void main(String[] args) {
+    int dayCount = 0;
+    
     static void summer() {
       System.out.println("This season is not ready yet, please check back later!");
     }
@@ -14,6 +16,22 @@ public class main {
     
     static void winter() {
       System.out.println("This season is not ready yet, please check back later!");
+    }
+  }
+  
+ static void dateTracker() { 
+    if (dayCount >= 365) {
+      int dayCount = 0
+    } else {
+      if (dayCount <= 92 && >=0) {
+        summer()
+      } else if (dayCount <= 184 && > 92) {
+        spring()
+      } else if (dayCount <= 275 && > 184) {
+        autumn()
+      } else if (dayCount <= 365 && > 275) {
+        winter()
+      }
     }
   }
 }
