7d0ac4c71d3176d5fdc21b3b26e97796cd7d2b32
Lukas Batema <lukasbatema@gmail.com>
2021-02-08 13:42:10 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,38 +1,75 @@
 public class main {
   public static void main(String[] args) {
-    System.out.println("This plugin is not ready yet, please check back later!");
+    dateTracker();
+    
+    // How many total ticks are in a day.
+    int dayLength = 12000;
+    
+    // How many total ticks there are in a night.
+    int nightLength = 12000;
+    
+    // Total ticks in a full day and night cycle.
+    int totalDayCycleLength = dayLength + nightLength;
   }
   
   static void summerSeason() {
-    System.out.println("This season is not ready yet, please check back later!");
+    while (dayCount >= 0 && < 92) {
+      // When at the highest dayLength, the day gains a total of 0.15 seconds.
+      double dayLength = dayLength + 0.05;
+      
+      // When at the lowest nightLength, the night loses a total of 0.15 seconds.
+      double nightLength = nightLength - 0.05;
+    }
   }
     
   static void springSeason() {
-    System.out.println("This season is not ready yet, please check back later!");
+    while (dayCount >= 93 && < 184) {
+      // When at the highest dayLength, the day gains a total of 0.15 seconds.
+      double dayLength = dayLength + 0.05;
+      
+      // When at the lowest nightLength, the night loses a total of 0.15 seconds.
+      double nightLength = nightLength - 0.05;
+    }
   }
     
   static void autumnSeason() {
-    System.out.println("This season is not ready yet, please check back later!");   
+    while (dayCount >= 185 && <= 275) {
+      // When at the lowest dayLength, the day gains a total of 3 seconds.
+      int dayLength = dayLength - 5;
+      
+      // When at the highest nightLength, the night loses a total of 3 seconds.
+      int nightLength = nightLength + 5;
+    }
   }
     
   static void winterSeason() {
-    System.out.println("This season is not ready yet, please check back later!");
+    while (dayCount >= 80 && <= 92) {
+      // When at the lowest dayLength, the day gains a total of 3 seconds.
+      int dayLength = dayLength - 5;
+      
+      // When at the highest nightLength, the night loses a total of 3 seconds.
+      int nightLength = nightLength + 5;
+    }
   }
   
  static void dateTracker() { 
    int dayCount = 0;
    
     if (dayCount >= 365) {
-      int dayCount = 0
+      int dayCount = 0;
     } else {
-      if (dayCount <= 92 && >=0) {
+      if (dayCount >= 0 && <=92) {
         summerSeason();
-      } else if (dayCount <= 184 && > 92) {
+      } else if (dayCount > 92 && <= 184) {
         springSeason();
-      } else if (dayCount <= 275 && > 184) {
+      } else if (dayCount > 184 && <= 275) {
         autumnSeason();
-      } else if (dayCount <= 365 && > 275) {
+      } else if (dayCount > 275 && <= 365) {
         winterSeason();
+      }
+      
+      while (summerTotalDayCycleLength > 23997 && <= 24000 || springTotalDayCycleLength > ) {
+        int dayCount++;
       }
     }
  }
