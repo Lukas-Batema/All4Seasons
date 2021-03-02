039c96396de717cb09862484c0ca2933953408e8
Lukas Batema <lukasbatema@gmail.com>
2021-03-02 12:49:50 -0500

    Update main.java

--- main.java
+++ main.java
@@ -2,76 +2,102 @@ package lukas-batema.mcje.plugin
 
 public class main {
   public static void main(String[] args) {
-    dateTracker();
-    
-    // How many total ticks are in a day.
-    int dayLength = 12000;
-    
-    // How many total ticks there are in a night.
-    int nightLength = 12000;
-    
-    // Total ticks in a full day and night cycle.
-    int totalDayCycleLength = dayLength + nightLength;
+    // Empty for now, will add more later.
   }
   
   static void summerSeason() {
-    while (dayCount >= 0 && < 92) {
-      // When at the highest dayLength, the day gains a total of 0.15 seconds.
+    // How many total ticks are in a day.
+    double dayLength = 12000;
+    
+    // How many total ticks there are in a night.
+    double nightLength = 12000;
+    
+    // Total ticks in a full day and night cycle.
+    double totalDayCycleLength = dayLength + nightLength;
+    
+    // Initial dayCount (Number of days passed).
+    double dayCount = 0;
+    
+    while (dayCount >= 0 && dayCount < 92) {
+      // When at the highest dayLength, the day gains a total of ___ seconds.
       double dayLength = dayLength + 0.05;
       
-      // When at the lowest nightLength, the night loses a total of 0.15 seconds.
+      // When at the lowest nightLength, the night loses a total of ___ seconds.
       double nightLength = nightLength - 0.05;
+      
+      dayCount++;
     }
   }
     
   static void springSeason() {
-    while (dayCount > 92 && <= 184) {
-      // When at the highest dayLength, the day gains a total of 0.15 seconds.
+    // How many total ticks are in a day.
+    double dayLength = 12000;
+    
+    // How many total ticks there are in a night.
+    double nightLength = 12000;
+    
+    // Total ticks in a full day and night cycle.
+    double totalDayCycleLength = dayLength + nightLength;
+    
+    // Initial dayCount (Number of days passed).
+    double dayCount = 0;
+    
+    while (dayCount > 92 && dayCount <= 184) {
+      // When at the highest dayLength, the day gains a total of ___ seconds.
       double dayLength = dayLength + 0.05;
       
-      // When at the lowest nightLength, the night loses a total of 0.15 seconds.
+      // When at the lowest nightLength, the night loses a total of ___ seconds.
       double nightLength = nightLength - 0.05;
+      
+      dayCount++;
     }
   }
     
   static void autumnSeason() {
-    while (dayCount > 184 && <= 275) {
-      // When at the lowest dayLength, the day gains a total of 3 seconds.
-      int dayLength = dayLength - 5;
+    // How many total ticks are in a day.
+    double dayLength = 12000;
+    
+    // How many total ticks there are in a night.
+    double nightLength = 12000;
+    
+    // Total ticks in a full day and night cycle.
+    double totalDayCycleLength = dayLength + nightLength;
+    
+    // Initial dayCount (Number of days passed).
+    double dayCount = 0;
+    
+    while (dayCount > 184 && dayCount <= 275) {
+      // When at the lowest dayLength, the day gains a total of ___ seconds.
+      double dayLength = dayLength - 5;
       
-      // When at the highest nightLength, the night loses a total of 3 seconds.
-      int nightLength = nightLength + 5;
+      // When at the highest nightLength, the night loses a total of ___ seconds.
+      double nightLength = nightLength + 5;
+      
+      dayCount++;
     }
   }
     
   static void winterSeason() {
-    while (dayCount > 184 && <= 365) {
-      // When at the lowest dayLength, the day gains a total of 30 seconds.
-      int dayLength = dayLength - 0.05;
+    // How many total ticks are in a day.
+    double dayLength = 12000;
+    
+    // How many total ticks there are in a night.
+    double nightLength = 12000;
+    
+    // Total ticks in a full day and night cycle.
+    double totalDayCycleLength = dayLength + nightLength;
+    
+    // Initial dayCount (Number of days passed).
+    double dayCount = 0;
+    
+    while (dayCount > 184 && dayCount <= 365) {
+      // When at the lowest dayLength, the day gains a total of ___ seconds.
+      double dayLength = dayLength - 0.05;
       
-      // When at the highest nightLength, the night loses a total of 30 seconds.
-      int nightLength = nightLength + 0.05;
+      // When at the highest nightLength, the night loses a total of ___ seconds.
+      double nightLength = nightLength + 0.05;
+      
+      dayCount++;
     }
   }
-  
- static void dateTracker() { 
-   int dayCount = 0;
-   
-    if (dayCount >= 365) {
-      int dayCount = 0;
-    } else {
-      if (dayCount >= 0 && <=91) {
-        summerSeason();
-      } else if (dayCount > 91 && <= 183) {
-        springSeason();
-      } else if (dayCount > 183 && <= 275) {
-        autumnSeason();
-      } else if (dayCount > 275 && <= 365) {
-        winterSeason();
-      }
-      
-      while (summerTotalDayCycleLength > 23997 && <= 24000 || springTotalDayCycleLength > ) {
-        int dayCount++;
-      }
-    }
- }
+}
